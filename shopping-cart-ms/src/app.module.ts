ebb3db0ae0b013698e1cc836211c7d08619ea655
GabrielGCardoso <gabrielguimac@gmail.com>
2021-04-12 09:10:32 -0300

    removing unuseful code

--- shopping-cart-ms/src/app.module.ts
+++ shopping-cart-ms/src/app.module.ts
@@ -1,12 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ShoppingCartModule } from './shopping-cart/shopping-cart.module';
-import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [ShoppingCartModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule { }
