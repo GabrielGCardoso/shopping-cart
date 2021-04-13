07659582630e7e9ad1fc0a933f24cf94cda2791a
GabrielGCardoso <gabrielguimac@gmail.com>
2021-04-13 09:15:48 -0300

    connection string and config

--- products-ms/src/app.module.ts
+++ products-ms/src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
+import Config from './config/config';
+const config = Config();
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [ProductsModule,
+    MongooseModule.forRoot(`mongodb://${config.database.username}:${config.database.password}@${config.database.host}:${config.database.port}`),
+  ],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- products-ms/src/config/config.ts
+++ products-ms/src/config/config.ts
@@ -1,7 +1,9 @@
 export default () => ({
     port: parseInt(process.env.NODE_PORT, 10) || 3002,
     database: {
-        host: process.env.NODE_ENV == 'DOCKER' ? 'mysql' : 'localhost',
-        port: 3306
+        host: process.env.NODE_ENV == 'DOCKER' ? 'mongo' : 'localhost',
+        port: 27017,
+        username: 'admin',
+        password: 'p1c4d1nh0',
     }
 });
